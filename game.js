1c76eb4f9bc9d91280184dc66b0d6e486e402eb4
Mark D. Boss <driftphantom@aol.com>
2014-11-06 16:49:26 -0500

    Update game.js
    local commit

--- game.js
+++ game.js
@@ -1,1 +1,28 @@
-//new
+  var runSomeTests = function(){
+
+        var descrip = document.getElementById('descrip');
+
+        console.log(descrip.tagName);
+        console.log(descrip.tagName);
+        console.log(descrip.parentNode.tagName);
+
+        var headings = document.querySelectorAll('aside > h1');
+        // any h1 element thats a child of an aside element
+        for (i = 0; i < headings.length; i++){
+        console.log(headlights[i].innerhtml);
+        }
+};
+
+window.onload = runSomeTests
+document.body.nodeName;
+
+document.getElementsByTagName("BUTTON"[0].textContent;
+// get the text content of the first button element
+
+
+element.toString()
+// Converts an element to a string
+
+
+
+
