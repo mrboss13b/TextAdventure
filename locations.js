f8a3b07dcfe5e7c94d83972f8d108cd9166d676d
Mark D. Boss <Mark.Boss1@marist.edu>
2014-12-14 21:09:21 -0500

    Update locations.js
    Scenes updated, now with 60 percent more humor.

--- locations.js
+++ locations.js
@@ -4,21 +4,21 @@ function Location(locationName,locationScene){
 	this.scene =locationScene;
 }
 
-var roomOne = new Location("Upper Tray", "grey plastic spires");
+var roomOne = new Location("Upper Tray", "grey plastic spires all around, doors to Scanner Housing and Roller Trap");
 
-var roomTwo = new Location("Scanner Housing", "plate glass terror");
+var roomTwo = new Location("Scanner Housing", "plate glass terror, doors to Upper Tray and Paper Storage");
 
-var roomThree = new Location("Colate Assembly", "the horror");
+var roomThree = new Location("Colate Assembly", "the horror, what does colate even mean? doors to Paper Storage and Toner Pit");
 
-var roomFour = new Location("Roller Trap", "cylinders of spinning death");
+var roomFour = new Location("Roller Trap", "cylinders of spinning death, and other poems, doors to Upper Tray, Paper Storage, and Cartridge Carrier");
 
-var roomFive = new Location("Paper Storage", "The great white void");
+var roomFive = new Location("Paper Storage", "The great white void, doors to Scanner Housing, Colate Assembly, Roller Trap, Cartridge Carrier and Toner Pit");
 
-var roomSix = new Location("Cartridge Carrier", "Insert fear");
+var roomSix = new Location("Cartridge Carrier", "Insert fear, or just shake it and pit it back, doors to Roller Trap and Paper Storage");
 
-var roomSeven = new Location("Toner Pit", "watch your footing");
+var roomSeven = new Location("Toner Pit", "Muck! No footing at all, doors to Colate Assembly Paper Storage and The Shredder");
 
-var roomEight = new Location("The Shredder", "It all comes to this");
+var roomEight = new Location("The Shredder", "It all comes to this, door to Toner Pit and Naked Puzzle Basement");
 
 var map = { 
 	locations : [ roomOne, roomTwo, roomThree, roomFour, roomFive, roomSix, roomSeven, roomEight ],
